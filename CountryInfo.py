40c03a875764b41f5b57deefedd99bbff284e62a
주언 <137984851+sulsikan@users.noreply.github.com>
2025-05-28 21:12:27 +0900

    Update CountryInfo.py

--- CountryInfo.py
+++ CountryInfo.py
@@ -4,7 +4,9 @@ from airflow.providers.postgres.hooks.postgres import PostgresHook
 from datetime import datetime
 from pandas import Timestamp
 
-import requests
+import yfinance as yf
+import pandas as pd
+import logging
 
 
 def get_Redshift_connection(autocommit=True):
@@ -13,32 +15,19 @@ def get_Redshift_connection(autocommit=True):
     conn.autocommit = autocommit
     return conn.cursor()
 
+
 @task
-def extract_transform():
-	 response = requests.get('https://restcountries.com/v3/all')
-	 countries = response.json()
-	 records = []
- 
-	 for country in countries:
-			 name = country['name']['offical']
-			 population = country['population']
-			 area = country['area']
-	 
-			 records.append([name, population, area])
-		return records
-		
+def get_historical_prices(symbol):
+    ticket = yf.Ticker(symbol)
+    data = ticket.history()
+    records = []
 
-def _create_table(cur, schema, table, drop_first):
-    if drop_first:
-        cur.execute(f"DROP TABLE IF EXISTS {schema}.{table};")
-        
-    cur.execute(f"""
-				CREATE TABLE {schema}.{table} (
-						name varchar(256) primary key,
-						population int,
-						area float
-				);""")
+    for index, row in data.iterrows():
+        date = index.strftime('%Y-%m-%d %H:%M:%S')
 
+        records.append([date, row["Open"], row["High"], row["Low"], row["Close"], row["Volume"]])
+
+    return records
 
 @task
 def load(schema, table, records):
@@ -46,33 +35,37 @@ def load(schema, table, records):
     cur = get_Redshift_connection()
     try:
         cur.execute("BEGIN;")
-        # 원본 테이블이 없으면 생성 - 테이블이 처음 한번 만들어질 때 필요한 코드
-        _create_table(cur, schema, table, False)
-        # 임시 테이블로 원본 테이블을 복사
-        cur.execute(f"CREATE TEMP TABLE t AS SELECT * FROM {schema}.{table};")
+        cur.execute(f"DROP TABLE IF EXISTS {schema}.{table};")
+        cur.execute(f"""
+CREATE TABLE {schema}.{table} (
+    date date,
+    "open" float,
+    high float,
+    low float,
+    close float,
+    volume bigint
+);""")
+        # DELETE FROM을 먼저 수행 -> FULL REFRESH을 하는 형태
         for r in records:
-            sql = f"INSERT INTO t VALUES ('{r[0]}', {r[1]}, {r[2]}, {r[3]}, {r[4]}, {r[5]});"
+            sql = f"INSERT INTO {schema}.{table} VALUES ('{r[0]}', {r[1]}, {r[2]}, {r[3]}, {r[4]}, {r[5]});"
             print(sql)
             cur.execute(sql)
-
-        # 원본 테이블 생성
-        _create_table(cur, schema, table, True)
-        # 임시 테이블 내용을 원본 테이블로 복사
-        cur.execute(f"INSERT INTO {schema}.{table} SELECT DISTINCT * FROM t;")
         cur.execute("COMMIT;")   # cur.execute("END;")
     except Exception as error:
         print(error)
-        cur.execute("ROLLBACK;") 
+        cur.execute("ROLLBACK;")
         raise
+
     logging.info("load done")
-    
+
+
 with DAG(
-		dag_id = 'CountryInfo',
-		start_date = datetime(2023,5,30),
-		catchup=False,
-		tags=['API'],
-		schedule = '30 6 * * 6' # 0 - Sunday, …, 6 - Saturday
+    dag_id = 'UpdateSymbol',
+    start_date = datetime(2023,5,30),
+    catchup=False,
+    tags=['API'],
+    schedule = '0 10 * * *'
 ) as dag:
 
-		results = extract_transform()
-		load("oju0916", "country_info", results)
+    results = get_historical_prices("AAPL")
+    load("keeyong", "stock_info", results)
